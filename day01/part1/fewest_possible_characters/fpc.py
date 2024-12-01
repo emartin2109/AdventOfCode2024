a410feed666e4d8e22c4049da289b26e1e3f2b23
Eliot <eliot1.martin@epitech.eu>
2024-12-01 17:14:05 +0100

    feat: reduce fpc files* to one line

--- day01/part1/fewest_possible_characters/fpc.py
+++ day01/part1/fewest_possible_characters/fpc.py
@@ -1,4 +1,1 @@
-import re
-import numpy as n
-a,b=map(lambda x:n.array(sorted(map(int,re.findall(x,open("i").read())))),[r"\d+ +",r" +\d+"])
-print(sum(abs(a-b)))
+import re;import numpy as n;a,b=map(lambda x:n.array(sorted(map(int,re.findall(x,open("i").read())))),[r"\d+ +",r" +\d+"]);print(sum(abs(a-b)))
