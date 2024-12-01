d14b8e90b38e449ba65b1d8522b7fa15c9d33b1a
Eliot <eliot1.martin@epitech.eu>
2024-12-01 16:55:54 +0100

    fix: remove useless sort from day 01 part 2

--- day01/part2/fewest_possible_characters/fpc.py
+++ day01/part2/fewest_possible_characters/fpc.py
@@ -1,3 +1,3 @@
 import re
-a,b=map(lambda x:sorted(map(int,re.findall(x,open("i").read()))),[r"\d+ +",r" +\d+"])
+a,b=map(lambda x:map(int,re.findall(x,open("i").read())),[r"\d+ +",r" +\d+"])
 print(sum(x*b.count(x)for x in a))
